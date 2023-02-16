0675cf4320510853b7c9c59675a3d5653f39ac53
Ajibola Junaid <jaeybee98@gmail.com>
2023-02-16 22:06:24 +0100

    Compute Area program.

---
+++ 100-days_challenge/day-01/ComputeArea.java
@@ -0,0 +1,18 @@
+class ComputeArea {
+	public static void main(String[] args) {
+		//Declare radius
+		double radius;
+
+		//Declare area
+		double area;
+		
+		//Assign a radius
+		radius = 20;
+
+		//Compute area
+		area = radius * radius * 3.14159;
+
+		//Display results
+		System.out.println("The area for the circle of radius " + radius + " is " + area);
+	}
+}
